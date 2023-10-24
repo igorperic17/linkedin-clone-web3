9ccdd6bf9aaca94e80106b2dcc9db30391bbea72
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-10-24 19:59:00 +0200

    add font

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,9 +1,9 @@
 import 'styles/globals.css'
-import type {AppProps} from 'next/app'
+import type { AppProps } from 'next/app'
 import Layout from 'components/Layout'
-import {SigningClientProvider} from 'contexts/client'
+import { SigningClientProvider } from 'contexts/client'
 
-function MyApp({Component, pageProps}: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SigningClientProvider>
       <Layout>

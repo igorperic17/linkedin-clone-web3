469048dabd261b84ec34f1c594ec8ae952aa5f0d
Ruben te Wierik <ruben.wierik@vista.com>
2023-10-24 14:59:11 +0200

    Implement deployment script.

---
+++ frontend/deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/zsh -e
+yarn build && yarn export
+
+OUT_DIR=out
+BUCKET=ebc9-frontend
+DISTRIBUTION_ID=ECKJZMB5XQ3I5
+
+aws s3 sync $OUT_DIR s3://$BUCKET/ \
+  --delete \
+  --exclude $OUT_DIR/sw.js \
+  --exclude "*.html" \
+  --metadata-directive REPLACE \
+  --cache-control max-age=31536000,public \
+  --acl public-read
+
+aws s3 cp $OUT_DIR s3://$BUCKET/ \
+  --exclude "*" \
+  --include "*.html" \
+  --include "$OUT_DIR/sw.js" \
+  --metadata-directive REPLACE \
+  --cache-control max-age=0,no-cache,no-store,must-revalidate \
+  --acl public-read \
+  --recursive
+
+process_html_file() {
+  file_path="$1"
+  relative_path="${file_path#$OUT_DIR/}"
+  file_name="${relative_path%.html}"
+
+  aws s3 cp s3://$BUCKET/$file_name.html s3://$BUCKET/$file_name
+}
+
+find $OUT_DIR -type f -name "*.html" | while read -r html_file; do
+  process_html_file "$html_file"
+done
+
+aws configure set preview.cloudfront true
+aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*"

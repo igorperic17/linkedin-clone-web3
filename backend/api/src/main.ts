70691e9ee0293696cd46d39e6b6cf7231ec08ae8
Ruben te Wierik <ruben.wierik@vista.com>
2023-10-25 21:46:05 +0200

    Add CORS settings to backend.

--- backend/api/src/main.ts
+++ backend/api/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: { origin: 'https://coredin.rubentewierik.dev', allowedHeaders: [ 'HEAD', 'OPTIONS', 'GET', 'POST'] }});
   await app.listen(3000);
 }
 bootstrap();

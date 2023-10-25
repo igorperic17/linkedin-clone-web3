454b77a7fb6a46f29b1d130015e21a56a503f5d8
Ruben te Wierik <ruben.wierik@vista.com>
2023-10-25 22:14:22 +0200

    Fixesd.

--- backend/api/src/main.ts
+++ backend/api/src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: { origin: 'https://coredin.rubentewierik.dev', allowedHeaders: [ 'HEAD', 'OPTIONS', 'GET', 'POST'] }});
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://coredin.rubentewierik.dev'
+  });
   await app.listen(3000);
 }
 bootstrap();
